8fd2cb494ad40f9f51bc22273d0e8a4e08b1ab7b
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-02-04 23:13:21 +0530

    Os info

---
+++ osinfo.js
@@ -0,0 +1,15 @@
+
+let os = require('os');
+console.log("Operating System Info:");
+
+console.log("OS Type: " + os.type());
+console.log(os.platform());
+console.log(os.arch());
+// console.log(os.cpus());
+
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.userInfo());
+
+
+
