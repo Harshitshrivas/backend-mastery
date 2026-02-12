ebec52dd9363a03bce126c9b15bc0593ef292ef2
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-02-13 00:09:40 +0530

    Update files

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/' , (req, res) =>{  // callback function jo har request pr call hoga
+    res.send("Hyyyyyy")
+})
+
+app.get('/about' , (req, res) =>{  // callback function jo har request pr call hoga
+    res.send("About Page")
+})
+
+app.get('/contact', (req, res) =>{
+    res.send("Contack page")
+})
+
+app.get('/:id' , (req, res) =>{    // dynamic route jo kisi bhi url pr match kr skta hai, jaise /123, /abc, etc.
+    res.send(`hey ${req.params.id}`)  // dynamic route 
+})
+
+app.listen(port , ()=>{    // jab server start ho jaye to ye message show hoga
+    console.log("Server Chalaaa dhapaa");
+})
