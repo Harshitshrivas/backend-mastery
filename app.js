ff086fcc5f868de2b31838427231b47c648d9901
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-02-13 23:52:38 +0530

    Express

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.get('/data/:id' , (req, res) =>{    // dynamic route jo kisi bhi url pr matc
     res.send(`hey ${req.params.id}`)  // dynamic route 
 })
 
+app.get("/search" , (req, res) =>{   // query parameter ko access krne ke liye
+    res.send(`Search for ${req.query.q}  and  ${req.query.category}`)  //  http://localhost:3000/search?q=javascript&category=programming
+})
+
 app.use(express.static('public'))  // static file serve krne ke liye, public folder ke andar jo bhi file hogi wo accessible hogi
 
 app.listen(port , ()=>{    // jab server start ho jaye to ye message show hoga
