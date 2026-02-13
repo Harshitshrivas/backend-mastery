3c50a1a07d07ca0903bf86b63f49832a33917ca3
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-02-13 10:53:50 +0530

    Express static file show

--- app.js
+++ app.js
@@ -15,9 +15,15 @@ app.get('/contact', (req, res) =>{
     res.send("Contack page")
 })
 
-app.get('/:id' , (req, res) =>{    // dynamic route jo kisi bhi url pr match kr skta hai, jaise /123, /abc, etc.
+// app.get('/:id' , (req, res) =>{    // dynamic route jo kisi bhi url pr match kr skta hai, jaise /123, /abc, etc.
+//     res.send(`hey ${req.params.id}`)  // dynamic route 
+// })
+
+app.get('/data/:id' , (req, res) =>{    // dynamic route jo kisi bhi url pr match kr skta hai, jaise /123, /abc, etc.
     res.send(`hey ${req.params.id}`)  // dynamic route 
 })
+
+app.use(express.static('public'))  // static file serve krne ke liye, public folder ke andar jo bhi file hogi wo accessible hogi
 
 app.listen(port , ()=>{    // jab server start ho jaye to ye message show hoga
     console.log("Server Chalaaa dhapaa");
