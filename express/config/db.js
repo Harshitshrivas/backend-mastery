ce0aa2d0213e81f58fa43c65355e6594ebc384e6
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-04-03 22:00:58 +0530

    Mongoose connect

---
+++ express/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const mongoDB = "mongodb://localhost:27017/my_database";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoDB);
+    //  mongoose.connect("mongodb://localhost:27017/my_database") direct bhi likh sakte
+    console.log("Mongodb Connected...");
+  } catch (error) {
+    console.log(error);
+  }
+};
