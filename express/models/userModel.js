5e9cba9d13305ee960de3492c6634bb775dcec2f
Harshit Shrivas <harshitshrivas88@gmail.com>
2026-05-06 11:13:53 +0530

    Monggose schema

--- express/models/userModel.js
+++ express/models/userModel.js
@@ -21,4 +21,4 @@ const userSchema = new mongoose.Schema({
 
 }, { timestamps: true }); // timestamps option se createdAt aur updatedAt fields automatically add ho jate hain, jo ki user ke creation aur update time ko track karte hain
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema); // userSchema se ek model create karna hai, jiska naam "User" hoga, aur usko export karna hai, taki hum usko controllers me use kar sake, jaise ki create, find, update, delete, etc.
